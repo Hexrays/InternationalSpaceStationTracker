cae9a7f2d36ea8385a1b8cc374b4f6005cd6045d
Jeffrey Denton <jeffd@hexrays.at>
2016-09-18 17:55:05 -0400

    styled map

--- client/components/Map.js
+++ client/components/Map.js
@@ -3,7 +3,33 @@ import GoogleMap from 'google-map-react';
 
 function createMapOptions() {
   return {
-    styles: [{ stylers: [{ 'saturation': -65 }, { 'gamma': 0.8 }, { 'lightness': 10 }, { 'visibility': 'on' }] }]
+    styles: [
+      {
+        "stylers": [
+          { "invert_lightness": true },
+          { "hue": "#4d00ff" },
+          { "gamma": 0.6 },
+          { "lightness": 9 },
+          { "saturation": -52 }
+        ]
+      },{
+        "featureType": "administrative.country",
+        "elementType": "labels.text",
+        "stylers": [
+          { "hue": "#9000ff" },
+          { "saturation": 12 },
+          { "lightness": -43 }
+        ]
+      },{
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "lightness": 2 },
+          { "hue": "#0022ff" },
+          { "saturation": -51 }
+        ]
+      }
+    ],
   };
 }
 
